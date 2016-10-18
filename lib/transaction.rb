eea5dd9904efe22553f53ac444b5bfcfcb0548fb
Tam <tamborinehands@gmail.com>
2016-10-18 12:04:54 +0100

    credit and debit can be specified on instantiation of transaction test pass

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,11 @@
 class Transaction
 
-  attr_reader :time
-  
+  attr_reader :time, :credit, :debit
+
     def initialize(credit = nil, debit = nil)
       @time = Time.now
+      @credit = credit
+      @debit = debit
     end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -11,9 +11,15 @@ it "timestamps upon instantiation" do
   expect(transaction.time).to be_within(10).of(Time.now)
 end
 
-it "credit can be specified on instantiation"
+it "credit can be specified on instantiation" do
+  transaction = Transaction.new(55)
+  expect(transaction.credit).to eq(55)
+end
 
-it "debit can be specified on instantiation"
+it "debit can be specified on instantiation" do
+  transaction = Transaction.new(0,55)
+  expect(transaction.debit).to eq(55)
+end
 
 it "debit and credit default to nil if not specified on instantiation"
 
