b39e83ea2d7690df8bb8048905cfb43c24ea02cd
Tam <tamborinehands@gmail.com>
2016-10-18 11:59:46 +0100

    skeleton transaction tests

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -11,5 +11,13 @@ it "timestamps upon instantiation" do
   expect(transaction.time).to be_within(10).of(Time.now)
 end
 
+it "credit can be specified on instantiation"
+
+it "debit can be specified on instantiation"
+
+it "debit and credit default to nil if not specified on instantiation"
+
+
+
 
 end
